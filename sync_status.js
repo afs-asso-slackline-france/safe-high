1758ed0c025806d5d31587627f570d5bcf8ddb9e
Marmotte Abbas <florent.paccalet@gmail.com>
2026-08-30 02:03:02 +0200

    remove localhost again

--- sync_status.js
+++ sync_status.js
@@ -5,7 +5,7 @@ function start() {
 				.map(item => item.dataset.coords);
 
 			const response = await fetch(
-				"http://localhost/routing.php?route=get_status",
+				"routing.php?route=get_status",
 				{
 					method: "POST",
 					headers: {
@@ -24,9 +24,9 @@ function start() {
 				document.getElementById("status_" + coords).querySelector(".status_message").innerHTML = statut;
 
 				if (statut.substring(0, 6) == "Stream") {
-					document.getElementById("status_" + coords).querySelector(".status_img").src = "http://localhost/img/anim_vert.gif";
+					document.getElementById("status_" + coords).querySelector(".status_img").src = "img/anim_vert.gif";
 				} else if (statut.substring(0, 3) == "XXX") {
-					document.getElementById("status_" + coords).querySelector(".status_img").src = "http://localhost/img/finish.png";
+					document.getElementById("status_" + coords).querySelector(".status_img").src = "img/finish.png";
 					const message = document.getElementById("status_" + coords)?.querySelector(".status_message");
 					message?.classList.remove("blink");
 				}
